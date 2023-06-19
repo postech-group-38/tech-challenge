b05e4baeaccbf3c34d15fb65adc84d5b893da924
wallace.benevides <wallace.benevides@b2wdigital.com>
2023-06-19 08:42:30 -0300

    chore: convert order to response

--- src/server/rest/controller/nest/order/search.ts
+++ src/server/rest/controller/nest/order/search.ts
@@ -1,6 +1,10 @@
 import { Controller, Logger, Get, Query } from '@nestjs/common';
 import { OrderService } from '../../../../../domain/service/order/index';
 import { OrderQuery } from '../../../model/request/order';
+import { OrderRespose } from '../../../model/response/order';
+import { Page } from '../../../../../domain/common/page';
+import { Order } from '../../../../../domain/model/order';
+import { OrderFilter } from '../../../../../domain/repository/order/filter';
 
 @Controller('/order')
 export class OrderSearchController {
@@ -10,6 +14,16 @@ export class OrderSearchController {
 
   @Get()
   async search(@Query() query: OrderQuery) {
-    return this.orderService.search(query.toFilter());
+    const filter = query.toFilter();
+    const orderPage = await this.orderService.search(filter);
+    return this.convertToResponse(orderPage, filter);
+  }
+
+  private convertToResponse(orderPage: Page<Order>, filter: OrderFilter) {
+    const orders = orderPage.items.map((order) =>
+      OrderRespose.fromDomain(order),
+    );
+
+    return new Page(orders, filter, orderPage.result.total);
   }
 }
